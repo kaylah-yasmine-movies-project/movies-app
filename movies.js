0e59a75109e77d0903cf9746dd4a5004d16eff92
Yasmine Rico <ricolyasmine@gmail.com>
2022-03-21 19:21:31 -0500

    add loop function to display to webpage and add pre-load functionality with an emoji

--- movies.js
+++ movies.js
@@ -1,3 +1,47 @@
-function getMovies(){
-    fetch("https://boom-protective-bambiraptor.glitch.me/movies").then(resp => resp.json()).then(data => console.log(data));
+"use strict"
+/**********************  loader ***********************/
+
+//https://javascript.plainenglish.io/adding-loader-to-your-deployed-projects-d8f389e8c928
+const loader = document.querySelector('.preload');
+const emoji = loader.querySelector('.emoji');
+
+const emojis = ["🕐", "🕜", "🕑","🕝", "🕒", "🕞", "🕓", "🕟", "🕔", "🕠", "🕕", "🕡", "🕖", "🕢",  "🕗", "🕣", "🕘", "🕤", "🕙",  "🕥", "🕚", "🕦",  "🕛", "🕧"];
+
+const interval = 125;
+
+const loadEmojis = (arr) => {
+    setInterval(() => {
+        emoji.innerText = arr[Math.floor(Math.random() * arr.length)];
+        //console.log(Math.floor(Math.random() * arr.length))
+    }, interval);
 }
+
+const init = () => {
+    loadEmojis(emojis);
+}
+init();
+
+
+    let movieArr = [];
+    let movieURL ='https://boom-protective-bambiraptor.glitch.me/movies';
+    //Create function movielover to include the fetch and movie data we want to append to html.
+    function movieLover () {
+        fetch(movieURL).then(response => response.json()).then(movies => {
+            movieArr = movies;
+            let html = '';
+
+            for (let movie of movies) {
+                html += `<h1 class=title>${movie.title}</h1>`
+                html += `<div class="rating">${movie.rating}</div>`
+            }
+
+            console.log(movies)
+            $('#container').append(html);
+        }).then(data => {
+            // console.log(data)
+            document.querySelector(".preload").style.display = "none"//stop the load
+        })
+    }
+    movieLover();
+
+
